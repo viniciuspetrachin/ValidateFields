f7e454c2dba3c7401cd9055f54c789479002bac3
Vinicius Petrachin <vr.petrachin@gmail.com>
2019-08-06 00:24:37 -0300

    - Added more setError - Added one more condition in password checking - Added comments to help beginners better understand the code.

--- app/src/main/java/com/github/viniciuspetrachin/validatefields/MainActivity.java
+++ app/src/main/java/com/github/viniciuspetrachin/validatefields/MainActivity.java
@@ -1,9 +1,20 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Vinícius Rodrigues Petrachin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.github.viniciuspetrachin.validatefields;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.ui_editText_password);
 
     }
+
     public void button_check_email(View view){
         ValidateFields validateFields = new ValidateFields(getApplicationContext());
 
@@ -30,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
     public void button_check_password(View view){
         ValidateFields validateFields = new ValidateFields(getApplicationContext());
 
@@ -40,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
     public void button_check_both(View view){
         ValidateFields validateFields = new ValidateFields(getApplicationContext());
         if(validateFields.isEmailPasswordValid(editTextEmail, editTextPassword)){
--- app/src/main/java/com/github/viniciuspetrachin/validatefields/ValidateFields.java
+++ app/src/main/java/com/github/viniciuspetrachin/validatefields/ValidateFields.java
@@ -1,37 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Vinícius Rodrigues Petrachin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.github.viniciuspetrachin.validatefields;
 
 import android.content.Context;
 import android.text.TextUtils;
 import android.widget.EditText;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidateFields {
 
     private Context context;
+    // You can substitute the regex code below for one that suits you best.
+    private Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=*])(?=\\S+$).*$");
+    // Set the minimum characters the password must contain.
+    private static final int PASSWORD_LENGTH = 8;
 
-    public ValidateFields(Context context) {
+    public ValidateFields() {
+
+    }
+
+    ValidateFields(Context context) {
         this.context = context;
     }
 
-    public boolean isEmailValid(EditText editText){
-         if(TextUtils.isEmpty(editText.getText().toString())){
-             editText.setError(context.getString(R.string.email_field_empty));
-             editText.requestFocus();
-             return false;
-         } else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(editText.getText().toString()).matches()){
-             editText.setError(context.getString(R.string.please_enter_a_valid_email));
-             editText.requestFocus();
-             return false;
-         } else{
-             return true;
-         }
+    // FUNCTIONS
+    boolean isEmailValid(EditText editText){
+        if(TextUtils.isEmpty(editText.getText().toString())){
+            editText.setError(context.getString(R.string.email_field_empty));
+            editText.requestFocus();
+            return false;
+        } else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(editText.getText().toString()).matches()){
+            editText.setError(context.getString(R.string.please_enter_a_valid_email));
+            editText.requestFocus();
+            return false;
+        } else{
+            return true;
+        }
     }
-    public boolean isPasswordValid(EditText editText){
-        Matcher matcher = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$")
-                .matcher(editText.getText().toString().trim());
-        if(!matcher.matches()){
+
+    boolean isPasswordValid(EditText editText){
+        String password = editText.getText().toString();
+        Matcher matcher = pattern.matcher(password.trim());
+        // Checks if password field is empty
+        if (password.equals("")) {
+            editText.setError(context.getString(R.string.password_field_empty));
+            editText.requestFocus();
+            return false;
+        }
+        // Checks whether the password field has the minimum required characters
+        else if (password.length() <= PASSWORD_LENGTH) {
+            editText.setError(context.getString(R.string.password_must_long) + " " + PASSWORD_LENGTH + " " + context.getString(R.string.characters));
+            editText.requestFocus();
+            return false;
+        }
+        // Checks whether the field meets regex requirements
+        else if (!matcher.matches()) {
             editText.setError(context.getString(R.string.password_must_contain));
             editText.requestFocus();
             return false;
@@ -39,7 +73,8 @@ public class ValidateFields {
             return true;
         }
     }
-    public boolean isEmailPasswordValid(EditText editTextEmail, EditText ediTextPassword){
+
+    boolean isEmailPasswordValid(EditText editTextEmail, EditText ediTextPassword){
         return isEmailValid(editTextEmail) && isPasswordValid(ediTextPassword);
     }
 
